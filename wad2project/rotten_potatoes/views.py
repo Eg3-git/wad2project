84fe544722e0ea008ab53a80322bae6966405df7
Filip Vyrostko <2479054V@student.gla.ac.uk>
2021-03-15 11:55:37 +0100

    Basic view functions add, waiting for database to be set up

--- wad2project/rotten_potatoes/views.py
+++ wad2project/rotten_potatoes/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from rotten_potatoes.models import*
+from rotten_potatoes.forms import*
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
 
-# Create your views here.
+
+def index(request):
+    pass
+
+
+def about(request):
+    return render(request, "rotten_potatoes/about.html")
+
+
+def register(request):
+    pass
+
+
+def user_login(request):
+    pass
+
+
+@login_required
+def user_logout(request):
+    pass
+
+
+def movie(request):
+    pass
+
+
+@login_required
+def edit_movie(request):
+    pass
+
+
+@login_required
+def add_comment(request):
+    pass
+
+
+@login_required
+def rate_movie(request):
+    pass
+
+
+@login_required
+def add_movie(request):
+    pass
+
+
+@login_required
+def account(request):
+    pass
+
+
+@login_required
+def edit_account(request):
+    pass
+
+
+def ratings(request):
+    pass
