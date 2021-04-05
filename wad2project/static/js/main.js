569178e6806c95869f609b5a4b6ee6a55eca83a3
Nikolaos Terzis <2479081t@student.gla.ac.uk>
2021-04-05 03:56:05 +0300

    Final modifications to css and js

---
+++ wad2project/static/js/main.js
@@ -0,0 +1,9 @@
+var chosenCommentId = null;
+
+function submitDeleteComment(cci){
+  document.getElementById(cci).submit();
+}
+
+function submitDeleteForm(){
+  document.getElementById('delete').submit();
+}
